cfb6d2a54ae143b9c3c3d1024eb12a9981b4616f
Luis Alberto Batalla Gonzalez <luisalbagoz@localhost.localdomain>
2025-04-02 07:30:01 -0600

    Reparaciones

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2/promise';
 // Configuración directa para MariaDB
 export const db = mysql.createPool({
     host: '127.0.0.1',       // Servidor de MariaDB (generalmente localhost)
-    user: 'root',            // Usuario de MariaDB
+    user: 'user1',            // Usuario de MariaDB
     password: 'pezcadofrito.1', // Contraseña del usuario
     database: 'agrivoltaica_db', // Nombre de la base de datos
 });
